2f93864c2893912b7a4b1a68a21c92eaa747d744
Trung Nguyen Nguyen <139950322+nguyennt72@users.noreply.github.com>
2024-06-09 10:15:14 +0700

    add validate shell

--- src/dto/shell.dto.ts
+++ src/dto/shell.dto.ts
@@ -1,7 +1,15 @@
+import { IsBoolean, IsNumber } from "class-validator"
+
 export class ShellDTO{
     ShellID: number
+    @IsNumber()
     ProductionCost: number
+    @IsBoolean()
     IsActive: boolean
+    @IsNumber()
     Weight: number
+    JewelryTypeID: number
+    MaterialJewelryID: number
+    MaterialID: number
     
 }
