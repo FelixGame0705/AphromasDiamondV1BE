34d4c3dee554676676bb4b3a96ebf210ae102256
Tran Tien <tientrangame0705@gmail.com>
2024-06-13 11:59:06 +0700

    set up cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { configSwagger } from './configs/api-docs.config';
 declare const module: any;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: true});
   configSwagger(app);
 
   await app.listen(3000);
