5764363368d04812f4894111f28bbf1400692082
Trung Nguyen Nguyen <139950322+nguyennt72@users.noreply.github.com>
2024-06-13 10:04:00 +0700

    api

--- src/dto/materaljewelry.dto.ts
+++ src/dto/materaljewelry.dto.ts
@@ -1,13 +1,24 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsDate, IsNumber, IsString } from "class-validator"
 
 export class MaterialJewelryDTO{
     MaterialID: number
+
+    @ApiProperty({ example: '12.000.000', description: 'Buy price' })
     @IsNumber()
     BuyPrice: number
+
+    @ApiProperty({ example: '12.000.000', description: 'Sell price' })
     @IsNumber()
     SellPrice: number
+
+    @ApiProperty({ example: '2023-06-07T14:30:00', description: 'Date update' })
     @IsDate()
     UpdateTime: Date
+
+    @ApiProperty
+    
+    ({ example: 'Gold 18k', description: 'Name' })
     @IsString()
     Name: string
 
