844bbd42660870430b6fa4f7a26c12b1317c962f
Tran Tien <tientrangame0705@gmail.com>
2024-07-23 12:20:36 +0700

    orderline

--- src/modules/orderline/orderLine.module.ts
+++ src/modules/orderline/orderLine.module.ts
@@ -4,9 +4,11 @@ import { OrderLineRepository } from "./orderLine.repository";
 import { OrderLineController } from "./orderLine.controller";
 import { OrderLineService } from "./orderLine.service";
 import { OrderLineEntity } from "src/entities/orderLine.entity";
+import { DiamondEntity } from "src/entities/diamond.entity";
+import { ProductEntity } from "src/entities/products.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([OrderLineEntity])],
+    imports: [TypeOrmModule.forFeature([OrderLineEntity, DiamondEntity, ProductEntity])],
     controllers: [OrderLineController],
     providers: [OrderLineService, {
         useClass: OrderLineRepository,
