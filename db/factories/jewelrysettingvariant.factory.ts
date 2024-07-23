910fc25c72f431b55925ed98a53b63f5acfdfc40
Trung Nguyen Nguyen <139950322+nguyennt72@users.noreply.github.com>
2024-07-24 06:54:33 +0700

    jw

--- db/factories/jewelrysettingvariant.factory.ts
+++ db/factories/jewelrysettingvariant.factory.ts
@@ -7,11 +7,13 @@ export const jewelrysettingvariantsFactory = setSeederFactory(JewelrySettingVari
     const  jewelrysettingvariants = new JewelrySettingVariantEntity()
 
 
-    jewelrysettingvariants.JewelrySettingID = faker.datatype.number({min: 1, max: 5});
-    jewelrysettingvariants.MaterialJewelryID = faker.datatype.number({min: 1, max: 4});
+    // jewelrysettingvariants.JewelrySettingID = faker.datatype.number({min: 1, max: 5});
+    // jewelrysettingvariants.MaterialJewelryID = faker.datatype.number({min: 1, max: 4});
+    
     jewelrysettingvariants.Weight = faker.datatype.number({min: 1, max: 5});
     jewelrysettingvariants.Quantity = faker.datatype.number({min: 1, max: 5});
     jewelrysettingvariants.Price = faker.datatype.number({min: 1, max: 5}); 
+
     // if(jewelrysettingvariants.MaterialJewelryID=1){ //Kiểm tra điều kiện nếu MaterialJewelryID là 1, thì gán giá trị cho trường SizeID.  
     // jewelrysettingvariants.SizeID = faker.datatype.number({min: 1, max: 15});
     // }else{
