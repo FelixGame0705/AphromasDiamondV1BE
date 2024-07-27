a11846b0e8576cb54660d4749bf6cec21dc0d0b8
Tran Tien <tientrangame0705@gmail.com>
2024-07-27 16:56:34 +0700

    filter product

--- src/modules/product/product.repository.ts
+++ src/modules/product/product.repository.ts
@@ -4,21 +4,21 @@ import { ProductEntity } from "src/entities/products.entity";
 import { BaseRepository } from "src/interfaces/BaseRepository";
 import { IProductRepository } from "src/interfaces/IProductRepository";
 import { Product } from "src/models/product.model";
-import { FindOptionsWhere, Repository } from "typeorm";
+import { Collection, FindOptionsWhere, In, Like, Repository } from "typeorm";
 import { DiamondRepository } from "../diamond/diamond.repository";
 import { DiamondEntity } from "src/entities/diamond.entity";
 
 @Injectable()
-export class ProductRepository extends BaseRepository<ProductEntity, Repository<ProductEntity>> implements IProductRepository{
+export class ProductRepository extends BaseRepository<ProductEntity, Repository<ProductEntity>> implements IProductRepository {
     constructor(
         @InjectRepository(ProductEntity)
         protected readonly repository: Repository<ProductEntity>
-    ){
+    ) {
         super(repository);
     }
     async findRelationById(id: number): Promise<ProductEntity> {
 
-        const rs = await this.repository.findOne({where: {[this.getIdField()]:id}, relations:['diamonds','usingImage','jewelrySetting','jewelrySetting.jewelrySettingVariant','jewelrySetting.jewelryType', 'diamonds.certificate', 'discount', 'diamonds.usingImage','jewelrySetting.usingImage', 'jewelrySetting.jewelrySettingVariant.materialJewelry']})
+        const rs = await this.repository.findOne({ where: { [this.getIdField()]: id }, relations: ['diamonds', 'usingImage', 'jewelrySetting', 'jewelrySetting.jewelrySettingVariant', 'jewelrySetting.jewelryType', 'diamonds.certificate', 'discount', 'diamonds.usingImage', 'jewelrySetting.usingImage', 'jewelrySetting.jewelrySettingVariant.materialJewelry'] })
         return rs;
         //return await this.repository.findOne({where: {[this.getIdField()]:id}, relations: ['diamonds']})
     }
@@ -29,7 +29,7 @@ export class ProductRepository extends BaseRepository<ProductEntity, Repository<
 
     async findAll(): Promise<ProductEntity[]> {
 
-        const dataRs = await this.repository.find({relations: ['diamonds', 'usingImage', 'discount', 'jewelrySetting', 'jewelrySetting.jewelrySettingVariant', 'jewelrySetting.jewelryType']})
+        const dataRs = await this.repository.find({ relations: ['diamonds', 'usingImage', 'discount', 'jewelrySetting', 'jewelrySetting.jewelrySettingVariant', 'jewelrySetting.jewelryType'] })
         // const builder = this.repository.createQueryBuilder('product')
         // .leftJoinAndSelect('product.diamonds', 'diamonds')
         // .leftJoinAndSelect('product.discount', 'discount')
@@ -38,7 +38,7 @@ export class ProductRepository extends BaseRepository<ProductEntity, Repository<
         // .leftJoinAndSelect('jewelrySetting.jewelrySettingVariant','JewelrySettingVariant')
         // .leftJoinAndSelect('jewelrySetting.jewelryType', 'jewelryType')
         // const data = await builder.getMany();
-        
+
         return dataRs;
     }
 
@@ -47,4 +47,106 @@ export class ProductRepository extends BaseRepository<ProductEntity, Repository<
     //     return await this.repository.findOne( {where: {[idField]:id} as FindOptionsWhere<ProductEntity>});
     // }
 
+    async paginateAndFilter(
+        page: number,
+        perPage: number,
+        filters: any,
+        sort: { field: string, order: 'ASC' | 'DESC' }
+    ): Promise<{ data: any[], total: number, page: number, last_page: number }> {
+
+        // Calculate skip value for pagination
+        const skip = (page - 1) * perPage;
+
+        // Create query options
+        const findOptions: any = {
+            relations: ['diamonds', 'usingImage', 'discount', 'jewelrySetting', 'jewelrySetting.jewelrySettingVariant', 'jewelrySetting.jewelryType'],
+            skip: skip,
+            take: perPage,
+            order: {}
+        };
+
+        // Apply sorting
+        if (sort && sort.field && sort.order) {
+            findOptions.order[sort.field] = sort.order;
+        }
+
+        // Apply filters
+        const whereConditions: any = {};
+
+        console.log('1979 FUCK ' + filters.DiamondShape[0]);
+        if (filters.Brand && filters.Brand.length > 0) {
+            whereConditions['Brand'] = In(filters.Brand);
+        }
+        if (filters.Collection && filters.Collection.length > 0) {
+            whereConditions['Collection'] = In(filters.Collection);
+        }
+        if (filters.Name ) {
+            whereConditions['Name'] = Like(`%${filters.Name}%`);
+        }
+
+
+        // Apply filters to find options
+        findOptions.where = whereConditions;
+
+        // Get filtered and sorted data
+        const data = await this.repository.find(findOptions);
+        let dataReturn = data;
+        if (filters.DiamondShape && filters.DiamondShape.length > 0) {
+            dataReturn = data.filter(product => {
+                console.log('Product Shapes:', product.jewelrySetting.DiamondShape);
+                return filters.DiamondShape.every(shape => {
+                    if (product.jewelrySetting.DiamondShape != null)
+                        return product.jewelrySetting.DiamondShape.includes(shape);
+                }
+                )
+            });
+        }
+
+        if (filters.JewelryType && filters.JewelryType.length > 0) {
+            dataReturn = dataReturn.filter(product =>
+                filters.JewelryType.every(type => {
+                    if (product.jewelrySetting.jewelryType.Name != null)
+                        return product.jewelrySetting.jewelryType.Name.includes(type)
+                }
+                )
+            );
+        }
+
+        // Get total count without pagination
+        const totalCount = dataReturn.length;
+
+        // Process data to include aggregated values
+        const enrichedData = dataReturn.filter(item => {
+            const totalDiamondPrice = item.diamonds.reduce((sum, diamond) => sum + diamond.Price, 0);
+            const firstVariantPrice = item.jewelrySetting.jewelrySettingVariant
+                ? Math.min(...item.jewelrySetting.jewelrySettingVariant.map(v => v.Price))
+                : 0;
+            const totalPrice = totalDiamondPrice + firstVariantPrice;
+
+            // Apply minPrice and maxPrice filters
+            return (!filters.minPrice || totalPrice >= filters.minPrice) &&
+                (!filters.maxPrice || totalPrice <= filters.maxPrice);
+        }).map(item => {
+            const totalDiamondPrice = item.diamonds.reduce((sum, diamond) => sum + diamond.Price, 0);
+            const firstVariantPrice = item.jewelrySetting.jewelrySettingVariant
+                ? Math.min(...item.jewelrySetting.jewelrySettingVariant.map(v => v.Price))
+                : 0;
+            return {
+                ...item,
+                TotalDiamondPrice: totalDiamondPrice,
+                FirstPrice: totalDiamondPrice + firstVariantPrice,
+            };
+        });
+
+
+
+        return {
+            data: enrichedData,
+            total: totalCount,
+            page,
+            last_page: Math.ceil(totalCount / perPage)
+        };
+    }
+
 }
+
