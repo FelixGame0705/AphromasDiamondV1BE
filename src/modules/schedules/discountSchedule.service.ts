c7dc3e0da7c9366c561095cb4f0402bc5349f2e8
Tran Tien <tientrangame0705@gmail.com>
2024-08-01 19:36:54 +0700

    optimize discount

--- src/modules/schedules/discountSchedule.service.ts
+++ src/modules/schedules/discountSchedule.service.ts
@@ -3,56 +3,61 @@ import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { DiscountEntity } from 'src/entities/discount.entity';
 import { ProductEntity } from 'src/entities/products.entity';
-import { Repository, Not } from 'typeorm';
+import { Repository, Not, LessThanOrEqual, MoreThanOrEqual, In, IsNull } from 'typeorm';
 import { DiamondEntity } from 'src/entities/diamond.entity';
+import { OrderLineEntity } from 'src/entities/orderLine.entity';
 
 @Injectable()
 export class DiscountScheduleService {
     constructor(
         @InjectRepository(DiscountEntity)
         private readonly discountRepository: Repository<DiscountEntity>,
-        @InjectRepository(ProductEntity)
-        private readonly productRepository: Repository<ProductEntity>,
         @InjectRepository(DiamondEntity)
         private readonly diamondRepository: Repository<DiamondEntity>,
-    ) {}
+        @InjectRepository(OrderLineEntity)
+        private readonly orderLineRepository: Repository<OrderLineEntity>,
+    ) { }
 
-    @Cron('0 0 * * * *', {
+    @Cron('* 0 * * * *', {
         name: 'schedule',
         //timeZone: 'Europe/Paris',
     })
     async triggerDiscount() {
-        const discountEntities = await this.discountRepository.find();
-        const productEntities = await this.productRepository.find();
-        const diamondEntities = await this.diamondRepository.find();
-        for (const discount of discountEntities) {
-            if (new Date(discount.StartDate).getTime() <= Date.now() && new Date(discount.EndDate).getTime() >= Date.now()) {
-                for (const product of productEntities) {
-                    if (product.DiscountID === discount.DiscountID) {
-                        // product.DiscountPrice = (product.Price * (1 - discount.PercentDiscounts / 100));
-                        await this.productRepository.save(product);
+        try {
+            const discountEntities = await this.discountRepository.find();
+            const diamondEntities = await this.diamondRepository.find({ where: { DiscountID: Not(null) } });
+            const orderlineEntities = await this.orderLineRepository.find({ where: { ProductID: Not(null), OrderID: Not(null) } })
+            for (const discount of discountEntities) {
+                if (new Date(discount?.StartDate).getTime() <= Date.now() && new Date(discount?.EndDate).getTime() >= Date.now()) {
+                    for (const orderlines of orderlineEntities) {
+                        if (orderlines.product.DiscountID === discount.DiscountID) {
+                            orderlines.DiscountPrice = (orderlines.Price) * (100 - discount.PercentDiscounts) / 100
+                            await this.orderLineRepository.save(orderlines);
+                        }
                     }
-                }
-                for (const diamond of diamondEntities) {
-                    if (diamond.DiscountID === discount.DiscountID) {
-                        diamond.DiscountPrice = (diamond.Price * (1 - discount.PercentDiscounts / 100));
-                        await this.diamondRepository.save(diamond);
+                    for (const diamond of diamondEntities) {
+                        if (diamond.DiscountID === discount.DiscountID) {
+                            diamond.DiscountPrice = (diamond.Price * (1 - discount.PercentDiscounts / 100));
+                            await this.diamondRepository.save(diamond);
+                        }
                     }
-                }
-            } else if (new Date(discount.EndDate).getTime() >= Date.now()) {
-                for (const product of productEntities) {
-                    if (product.DiscountID === discount.DiscountID) {
-                        // product.DiscountPrice = null; // Assuming you want to remove the discount price
-                        await this.productRepository.save(product);
+                } else if (new Date(discount?.EndDate).getTime() >= Date.now()) {
+                    for (const orderlines of orderlineEntities) {
+                        if (orderlines.product.DiscountID === discount.DiscountID) {
+                            orderlines.DiscountPrice = orderlines.Price;
+                            await this.orderLineRepository.save(orderlines);
+                        }
                     }
-                }
-                for (const diamond of diamondEntities) {
-                    if (diamond.DiscountID === discount.DiscountID) {
-                        diamond.DiscountPrice = null; // Assuming you want to remove the discount price
-                        await this.diamondRepository.save(diamond);
+                    for (const diamond of diamondEntities) {
+                        if (diamond.DiscountID === discount.DiscountID) {
+                            diamond.DiscountPrice = diamond.Price; // Assuming you want to remove the discount price
+                            await this.diamondRepository.save(diamond);
+                        }
                     }
                 }
             }
+        }catch(error){
+            console.log(error)
         }
     }
 }
